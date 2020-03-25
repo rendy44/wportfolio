7337c119b77f81789b14e50520ba35fe425ce7df
rendy44 <rendy.de.p@gmail.com>
2020-03-25 09:39:47 +0700

    Modify section open default size

--- wp-content/themes/wportfolio/templates/global/section-open.php
+++ wp-content/themes/wportfolio/templates/global/section-open.php
@@ -4,7 +4,7 @@
  *
  * @author WPerfekt
  * @package WPortfolio
- * @version 0.0.2
+ * @version 0.0.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $section_class = isset( $section_class ) && $section_class ? $section_class : '';
-$section_size  = isset( $section_size ) && $section_size ? $section_size : '2-3';
+$section_size  = isset( $section_size ) && $section_size ? $section_size : 'col-sm-2-3';
 ?>
 
 <section class="section <?php echo esc_attr( $section_class ); ?>" id="<?php echo esc_attr( $section_id ); ?>">
@@ -20,4 +20,4 @@ $section_size  = isset( $section_size ) && $section_size ? $section_size : '2-3'
     <div class="right-section"></div>
     <div class="frow-container">
         <div class="frow">
-            <div class="col-sm-<?php echo esc_attr( $section_size ); ?>">
+            <div class="<?php echo esc_attr( $section_size ); ?>">
